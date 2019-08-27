45feced0b0425f84e13924f57af00b68fe96155d
Myung-gwan Kim <cartopia95@naver.com>
2019-08-27 19:38:38 +0900

    사용자가 게시한 게시물 리스트 조회 라우터 추가

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,21 +1,26 @@
 import express from "express"
-import { findAllPost, findPost, createPost, modifyPost, deletePost } from "../controller/postController"
+import { findAllPost, findPost, createPost, modifyPost, deletePost, findPostByUserId } from "../controller/postController"
 
 const router = express.Router();
 
 // 모든 게시물 조회
 router.get("/findAllPost", findAllPost);
 
-// post.id로 해당 게시물 조회
+// req.params.id에 있는 post.id로 해당 게시물 조회
 router.get("/findPost/:id", findPost);
+
+// req.user안에 들어있는 req.user.id로 게시한 게시물 조회
+router.get("/findPostByUserId", findPostByUserId);
+
+// user.id로 스크랩한 게시물 조회
 
 // 게시물 등록
 router.post("/createPost", createPost);
 
-// post.id로 해당 게시물 수정
+// req.params.id에 있는 post.id로 해당 게시물 수정
 router.put("/modifyPost/:id", modifyPost);
 
-// post.id로 해당 게시물 삭제
+// req.params.id에 있는 post.id로 해당 게시물 삭제
 router.delete("/deletePost/:id", deletePost);
 
 module.exports = router;
