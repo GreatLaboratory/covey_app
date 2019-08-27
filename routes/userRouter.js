546260061d91969568371c1065652c74b5ecee7c
Myung-gwan Kim <cartopia95@naver.com>
2019-08-27 17:17:59 +0900

    회원정보 조회 url변경

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,28 +3,11 @@ import { findAllUser, createUser, findUser, modifyUser, deleteUser } from "../co
 
 const router = express.Router();
 
-/**
- * @swagger
- * definitions:
- *   Users:
- *     required:
- *       - id
- *     properties:
- *       id:
- *         type: integer
- *       name:
- *         type: string
- *       snsId:
- *         type: string
- *       email:
- *         type: string
- */
-
 // 모든 회원 목록 조회
 router.get("/findAllUser", findAllUser);
 
 // user id로 해당 회원 조회
-router.get("/findByPk/:id", findUser);
+router.get("/findUser/:id", findUser);
 
 // req.body에 받은 json을 새로운 회원에 추가
 router.post("/createUser", createUser);
