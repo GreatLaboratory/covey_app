f98581cf383a090e8731d66fccfe1ce3328925fa
Myung-gwan Kim <cartopia95@naver.com>
2019-09-04 17:56:58 +0900

    대타 신청기능에 로그인이 필수로 설정

--- routes/applyRouter.js
+++ routes/applyRouter.js
@@ -1,16 +1,17 @@
 import express from "express"
-
 import { findAllApplied, applyPost, cancelApply} from "../controller/applyController"
+
+const { isLoggedIn } = require("./middleWares");
 const router = express.Router();
 
 // 게시물 지원하기
-router.post("/applyPost/:postId", applyPost);
+router.post("/applyPost/:postId", isLoggedIn, applyPost);
 
 // req.user.id로 자신이 지원한 게시물 리스트 조회
-router.get("/findAllApplied", findAllApplied);
+router.get("/findAllApplied", isLoggedIn, findAllApplied);
 
 // 지원 취소
-router.delete("/cancelApply/:postId", cancelApply);
+router.delete("/cancelApply/:postId", isLoggedIn, cancelApply);
 
 // 선택-매칭하기
 
