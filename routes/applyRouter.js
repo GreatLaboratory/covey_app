ff1ee8bad6c2183c5653929303a28c97b9c1c9ec
Myung-gwan Kim <cartopia95@naver.com>
2019-09-05 19:24:37 +0900

    지원자-게시자 매칭 라우터 추가

--- routes/applyRouter.js
+++ routes/applyRouter.js
@@ -1,19 +1,19 @@
 import express from "express"
-import { findAllApplied, applyPost, cancelApply} from "../controller/applyController"
+import { findAllApplied, applyPost, cancelApply, matching} from "../controller/applyController"
 
 const { isLoggedIn } = require("./middleWares");
 const router = express.Router();
 
 // 게시물 지원하기
-router.post("/applyPost/:postId", isLoggedIn, applyPost);
+router.post("/:postId", isLoggedIn, applyPost);
 
 // req.user.id로 자신이 지원한 게시물 리스트 조회
 router.get("/findAllApplied", isLoggedIn, findAllApplied);
 
 // 지원 취소
-router.delete("/cancelApply/:postId", isLoggedIn, cancelApply);
+router.delete("/cancel/:postId", isLoggedIn, cancelApply);
 
 // 선택-매칭하기
-
+router.put("/matching/:postId/:userId", matching);
 
 module.exports = router;
