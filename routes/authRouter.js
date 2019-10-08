0bf3c959ab19809f539aaed73139411987873daa
Myung-gwan Kim <cartopia95@naver.com>
2019-10-08 14:11:36 +0900

    페이스북, 휴대폰인증 라우터 추가

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,22 +1,29 @@
 import express from "express"
 import passport from "passport"
+import { logout, sendCodeToPhone, verifyCode } from "../controller/authController";
 
 const { isLoggedIn } = require("./middleWares");
 const router = express.Router();
 
-// 카카오 로그인 창으로 리다이렉트
+// 이렇게 요청하면 카카오 로그인 창으로 리다이렉트가 간다.
 router.get("/kakao", passport.authenticate("kakao"));
 
 // 로그인 결과를 받는다.
 router.get("/kakao/callback", passport.authenticate("kakao", {failureRedirect:"/"}), (req, res) => { res.json(req.user) });
 
+// 이렇게 요청하면 페이스북 로그인창으로 리다이렉트가 간다.
+router.get("/facebook", passport.authenticate("facebook"));
+
+// 로그인 결과를 받는다.
+router.get("/facebook/callback", passport.authenticate("facebook", {failureRedirect:"/"}), (req, res) => { res.json(req.user) });
+
 // 로그아웃
-router.get("/logout", isLoggedIn, (req, res)=>{
-    req.logout();  // req.user객체를 제거하는 역할
-    req.session.destroy();  // req.session 객체내용 제거 역할
-    res.json({ message : "로그아웃에 성공하였습니다."});
-});
+router.get("/logout", isLoggedIn, logout);
+
+// 폰번호로 인증번호 발송
+router.post('/phone', sendCodeToPhone);
+
+// 인증번호 인증
+router.post('/verify', verifyCode);
 
 module.exports = router;
-
-// 콜백함수가 따로 필요 없기 때문에 controller 불필요
