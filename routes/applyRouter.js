342b89c801f7fc2f291719b01739e1b16d651355
Myung-gwan Kim <cartopia95@naver.com>
2019-11-01 20:08:18 +0900

    swagger 지원 관련 api 추가

--- routes/applyRouter.js
+++ routes/applyRouter.js
@@ -5,15 +5,65 @@ const { isLoggedIn } = require("./middleWares");
 const router = express.Router();
 
 // 게시물 지원하기
-router.post("/:postId", isLoggedIn, applyPost);
-
-// req.user.id로 자신이 지원한 게시물 리스트 조회
-router.get("/appliedList", isLoggedIn, findAllApplied);
+router.post("/:postId", applyPost);
 
 // 지원 취소
-router.delete("/:postId", isLoggedIn, cancelApply);
+router.delete("/:postId", cancelApply);
 
-// 선택-매칭하기
-// router.put("/match/:postId/:userId", matching);
+// req.user.id로 자신이 지원한 게시물 리스트 조회
+router.get("/appliedList", findAllApplied);
 
+/**
+ * @swagger
+ * tags:
+ *   name: ApplyRouter
+ *   description: 지원관련 api
+ * /api/apply/{postId}:
+ *   post:
+ *     tags:
+ *       - ApplyRouter
+ *     summary: 게시물에 지원하기
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: postId
+ *         in: path
+ *         description: 지원할 게시물 번호
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       201:
+ *         description: 지원 성공
+ *       404:
+ *         description: Not Found
+ *   delete:
+ *     tags:
+ *       - ApplyRouter
+ *     summary: 게시물에 지원 취소하기
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: postId
+ *         in: path
+ *         description: 지원 취소할 게시물 번호
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       204:
+ *         description: 삭제 성공
+ *       404:
+ *         description: Not Found
+ * /api/apply/appliedList:
+ *   get:
+ *     tags:
+ *       - ApplyRouter
+ *     summary: 로그인된 사용자가 지원한 게시물 리스트 조회
+ *     consumes:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: 조회 성공
+ *       404:
+ *         description: Not Found
+ */
 module.exports = router;
