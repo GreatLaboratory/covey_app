f312fad56c9e2ce4a9af2c46754ca45938c98bf3
Myung-gwan Kim <cartopia95@naver.com>
2019-11-01 20:08:00 +0900

    swagger 사용자 관련 api 추가

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,13 +1,11 @@
 import express from "express"
 import {
-    addUserInfo,
     findUser,
     modifyUser,
     deleteUser,
     upload,
     findApplicant,
     findAuthorizedUser,
-    addCareer
 } from "../controller/userController"
 import fs from "fs";
 
@@ -39,12 +37,102 @@ fs.readdir('uploads', (error) => {
 // router.put("/signUp", addUserInfo);
 
 // req.user.id로 현재 로그인된 회원 정보 수정
-router.put("/", isLoggedIn, modifyUser);
+router.put("/", modifyUser);
 
 // req.user.id로 현재 로그인된 회원 탈퇴
-router.delete("/", isLoggedIn, deleteUser);
+router.delete("/", deleteUser);
 
-// 경력사항 추가
-router.post('/career', addCareer);
-
+/**
+ * @swagger
+ * tags:
+ *   name: UserRouter
+ *   description: 사용자 관련 api
+ * /api/user:
+ *   get:
+ *     tags:
+ *       - UserRouter
+ *     summary: 로그인된 사용자의 프로필 정보 조회
+ *     consumes:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: 조회 성공
+ *       404:
+ *         description: Not Found
+ *   put:
+ *     tags:
+ *       - UserRouter
+ *     summary: 로그인된 사용자의 프로필 정보 수정
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         description: |
+ *           프로필 정보 수정할 때 request body에 넣어줘야할 json
+ *           1. 수정하려는 내용의 key만 선별해서 json작성
+ *           2. "id", "snsId","img", "phoneNum" 이렇게 4개 key들은 body에서 제외해야함.
+ *           3. address1는 시/도, address2는 구/시 (ex:서울특별시 / 중랑구)
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/User'
+ *     responses:
+ *       201:
+ *         description: 수정 성공
+ *       400:
+ *         description: 잘못된 request body
+ *       404:
+ *         description: Not Found
+ *   delete:
+ *     tags:
+ *       - UserRouter
+ *     summary: 로그인된 사용자의 회원탈퇴
+ *     consumes:
+ *       - application/json
+ *     responses:
+ *       204:
+ *         description: 삭제 성공
+ *       404:
+ *         description: Not Found
+ * /api/user/applicants/{postId}:
+ *   get:
+ *     tags:
+ *       - UserRouter
+ *     summary: 게시물에 지원한 사용자들의 이름과 핸드폰 번호의 리스트 조회
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: postId
+ *         in: path
+ *         description: 게시물 번호
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: 게시자의 조회 성공
+ *       204:
+ *         description: 비게시자의 조회 성공
+ *       404:
+ *         description: Not Found
+ * /api/user/others/{userId}:
+ *   get:
+ *     tags:
+ *       - UserRouter
+ *     summary: 지원자 or 게시자의 프로필 정보 조회
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: userId
+ *         in: path
+ *         description: 사용자 번호
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: 조회 성공
+ *       400:
+ *         description: 잘못된 파라미터 형식의 요청
+ *       404:
+ *         description: Not Found
+ */
 module.exports = router;
