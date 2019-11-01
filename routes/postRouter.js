24b9c3f240bd4eaead26f7f7855b2349a31f84ca
Myung-gwan Kim <cartopia95@naver.com>
2019-11-01 20:08:54 +0900

    swagger 게시물 관련 api 추가

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -40,19 +40,18 @@ router.delete("/:postId", deletePost);
  * @swagger
  * tags:
  *   name: PostRouter
- *   description: /api/post/
+ *   description: 게시물 관련 api
  * /api/post/list/{page}:
  *   get:
  *     tags:
  *       - PostRouter
- *     name: QueryList
  *     summary: 필터링 거친 게시물 리스트 조회 (+페이징)
  *     consumes:
  *       - application/json
  *     parameters:
  *       - name: page
  *         in: path
- *         description: 페이지
+ *         description: 페이지 번호
  *         required: true
  *         type: integer
  *       - name: pay
@@ -84,23 +83,126 @@ router.delete("/:postId", deletePost);
  *         type: string
  *       - name: startDate
  *         in: query
- *         description: 시작날짜
+ *         description: 시작날짜 (ex:2019-09-30)
  *         required: true
  *         type: string
  *         format: date
  *       - name: endDate
  *         in: query
- *         description: 끝날짜
+ *         description: 끝날짜 (ex:2019-09-30)
  *         required: true
  *         type: string
  *         format: date
  *     responses:
  *       200:
- *         description: User found and logged in successfully
- *       401:
- *         description: Bad username, not found in db
- *       403:
- *         description: Username and password don't match
+ *         description: 리스트 조회 성공
+ *       400:
+ *         description: 잘못된 파라미터
+ *       404:
+ *         description: Not Found
+ * /api/post/payList/{page}:
+ *   get:
+ *     tags:
+ *       - PostRouter
+ *     summary: 시급 높은 순서대로 게시물 리스트 조회(+페이징) -> 고수익 대타
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: page
+ *         in: path
+ *         description: 페이지 번호
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: 리스트 조회 성공
+ *       400:
+ *         description: 잘못된 파라미터
+ *       404:
+ *         description: Not Found
+ * /api/post/addressList/{page}:
+ *   get:
+ *     tags:
+ *       - PostRouter
+ *     summary: 주소(시와 구)가 같은 게시물 리스트 조회(+페이징) -> 우리동네 대타
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: page
+ *         in: path
+ *         description: 페이지 번호
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: 리스트 조회 성공
+ *       400:
+ *         description: 잘못된 파라미터
+ *       404:
+ *         description: Not Found
+ * /api/post/{postId}:
+ *   get:
+ *     tags:
+ *       - PostRouter
+ *     summary: 대타 게시물 상세정보 조회
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: postId
+ *         in: path
+ *         description: 게시물 고유 index (primary key)
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: 조회 성공
+ *       404:
+ *         description: Not Found
+ *   put:
+ *     tags:
+ *       - PostRouter
+ *     summary: 대타 게시물 수정하기
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: postId
+ *         in: path
+ *         description: 게시물 번호
+ *         required: true
+ *         type: integer
+ *       - name: body
+ *         in: body
+ *         description: |
+ *           게시물 수정할 때 request body에 넣어줘야할 json
+ *           1. 수정하려는 내용의 key만 선별해서 json작성
+ *           2. "id", "userId","img1, 2, 3" 이렇게 5개 key들은 body에서 제외해야함.
+ *           3. 날짜는 2019-09-30 이 형식으로
+ *           4. workingTime은 11:00 ~ 20:00 이 형식으로
+ *           5. address1는 시/도, address2는 구/시, address3는 도로명 및 상세주소 (ex:서울특별시 / 중랑구 / 동일로136길 15)
+ *           6. category는 "CAFE", "RESTAURANT", "PC", "ETC" 중에 하나로
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Post'
+ *     responses:
+ *       201:
+ *         description: 수정 성공
+ *       404:
+ *         description: Not Found
+ *   delete:
+ *     tags:
+ *       - PostRouter
+ *     summary: 대타 게시물 삭제하기
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: postId
+ *         in: path
+ *         description: 게시물 번호
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       204:
+ *         description: 삭제 성공
  *       404:
  *         description: Not Found
  * /api/post/registerList:
@@ -108,17 +210,42 @@ router.delete("/:postId", deletePost);
  *     tags:
  *       - PostRouter
  *     name: registerList
- *     summary: 로그인된 사용자가 게시한 게시물 목록 조회
+ *     summary: 로그인된 사용자가 게시한 게시물 목록 조회 (404 무한에러 아직 해결 못함 - 미완성)
  *     consumes:
  *       - application/json
  *     responses:
  *       200:
- *         description: User found and logged in successfully
- *       401:
- *         description: Bad username, not found in db
- *       403:
- *         description: Username and password don't match
+ *         description: 리스트 조회 성공
  *       404:
  *         description: Not Found
+ * /api/post:
+ *   post:
+ *     tags:
+ *       - PostRouter
+ *     summary: 대타 게시물 등록하기
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         description: |
+ *           게시물 등록할 때 request body에 넣어줘야할 json
+ *           1. "id", "userId","img1, 2, 3" 이렇게 5개 key들은 body에서 제외해야함.
+ *           2. 날짜는 2019-09-30 이 형식으로
+ *           3. workingTime은 11:00 ~ 20:00 이 형식으로
+ *           4. address1는 시/도, address2는 구/시, address3는 도로명 및 상세주소 (ex:서울특별시 / 중랑구 / 동일로136길 15)
+ *           5. category는 "CAFE", "RESTAURANT", "PC", "ETC" 중에 하나로
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Post'
+ *     responses:
+ *       201:
+ *         description: 등록 성공
+ *       400:
+ *         description: 잘못된 요청바디
+ *       404:
+ *         description: Not Found
+ *       409:
+ *         description: 중복된 제목 충돌
  */
 module.exports = router;
