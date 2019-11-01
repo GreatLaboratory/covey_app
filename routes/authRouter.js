862f57c1924bbc4f97b9bb80af38e1d5bfa2abfb
Myung-gwan Kim <cartopia95@naver.com>
2019-11-01 20:08:44 +0900

    swagger 로그인, 회원가입 관련 api 추가

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -23,7 +23,72 @@ router.get("/logout", isLoggedIn, logout);
 // 폰번호로 인증번호 발송
 router.post('/phone', sendCodeToPhone);
 
-// 인증번호 인증
+// 인증번호 인증-
 router.post('/verify', verifyCode);
 
+/**
+ * @swagger
+ * tags:
+ *   name: AuthRouter
+ *   description: 회원가입 & 로그인관련 api
+ * /api/auth/logout:
+ *   get:
+ *     tags:
+ *       - AuthRouter
+ *     summary: 로그아웃하기
+ *     consumes:
+ *       - application/json
+ *     responses:
+ *       204:
+ *         description: 로그아웃 성공
+ *       403:
+ *         description: 로그인 필요
+ *       404:
+ *         description: Not Found
+ * /api/auth/phone:
+ *   post:
+ *     tags:
+ *       - AuthRouter
+ *     summary: 폰번호로 인증번호 발송
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         description: |
+ *           핸드폰 번호를 json형식으로 request body에 넣어줘야함.
+ *           {
+ *              "phoneNum" : "01092988726"
+ *           }
+ *         required: true
+ *     responses:
+ *       201:
+ *         description: 인증번호 발송 성공
+ *       404:
+ *         description: Not Found
+ * /api/auth/verify:
+ *   post:
+ *     tags:
+ *       - AuthRouter
+ *     summary: 인증번호 인증하기
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         description: |
+ *           핸드폰 번호와 인증번호를 json형식으로 request body에 넣어줘야함.
+ *           {
+ *              "phoneNum" : "01092988726",
+ *              "verifyNumFromClient" : 4561237858
+ *           }
+ *         required: true
+ *     responses:
+ *       201:
+ *         description: 인증 성공
+ *       401:
+ *         description: 인증 실패
+ *       404:
+ *         description: Not Found
+ */
 module.exports = router;
