50185ca3ad2fbea557e3cfd3ae70f22accafbd9f
Myung-gwan Kim <cartopia95@naver.com>
2019-11-02 21:29:31 +0900

    더미 데이터 변경

--- seeders/20191011125658-post.js
+++ seeders/20191011125658-post.js
@@ -4,7 +4,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     let datas = [];
     for(let i = 0; i < 10; i++){
-      const category_arr = ["CAFE", "RESTAURANT", "PC", "ETC"];
+      const category_arr = ["식당", "카페", "술집", "편의점", "잡화매장", "독서실", "PC방", "기타"];
       const address2_arr = ["강남구", "송파구", "강동구", "종로구", "중랑구"];
       let obj = {
         userId: 1,
@@ -12,13 +12,14 @@ module.exports = {
         startDate: "2019-10-01",
         endDate: "2019-10-15",
         dueDate: "2019-09-25",
+        isDue: 0,
         workingTime: "11:00 ~ 20:00",
         address1: "서울특별시",
         address2: address2_arr[i%5],
         address3: "미사강변대로 165",
         pay: 8500 + (1000*i),
         description: "testDesc" + (i+1),
-        category: category_arr[i%4],
+        category: category_arr[i%8],
         createdAt: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
         updatedAt: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
       };
