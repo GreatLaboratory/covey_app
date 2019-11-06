2baa9e585111e06e4a84ec0db2e52307b085e816
Myung-gwan Kim <cartopia95@naver.com>
2019-11-07 08:57:28 +0900

    이미지 업로드를 위한 미들웨어 추가

--- routes/middleWares.js
+++ routes/middleWares.js
@@ -1,8 +1,11 @@
+import multer from 'multer'
+import path from 'path'
+
 exports.isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) {
         next();
     } else {
-        res.status(403).send("로그인 필요");
+        res.status(403).json({ message : "로그인 필요" });
     }
 };
 
@@ -13,3 +16,15 @@ exports.isNotLoggedIn = (req, res, next)=> {
         res.redirect("/");
     }
 };
+
+exports.upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, 'uploads/');
+        },
+        filename: function (req, file, cb) {
+            const ext = path.extname(file.originalname);
+            cb(null, path.basename(file.originalname, ext) + new Date().valueOf() + ext);
+        }
+    }),
+});
