1860a4638a920104ed4c9154e73507179ebb077f
Myung-gwan Kim <cartopia95@naver.com>
2019-11-11 22:11:09 +0900

    사용자 프로필 이미지 업로드 라우터 추가

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,13 +3,12 @@ import {
     findUser,
     modifyUser,
     deleteUser,
-    upload,
     findApplicant,
     findAuthorizedUser,
 } from "../controller/userController"
 import fs from "fs";
 
-const { isLoggedIn } = require("./middleWares");
+const { isLoggedIn, upload } = require("./middleWares");
 const router = express.Router();
 
 // req.user.id로 현재 로그인된 회원 조회
@@ -21,23 +20,8 @@ router.get("/applicants/:postId", findApplicant);
 // (지원자 or 게시자)의 프로필 조회
 router.get("/others/:userId", findUser);
 
-// img 업로드
-fs.readdir('uploads', (error) => {
-    if (error) {
-        console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-        fs.mkdirSync('uploads');
-    }
-});
-// router.post("/img", upload.single(), (req, res) => {
-//     console.log(req.file);
-//     res.json({ url: `/img/${req.file.filename}` });
-// });
-
-// sns로그인 직후 추가로 회원정보 받기  -> 이건 밑에 있는 라우터로도 얼마든지 가능함
-// router.put("/signUp", addUserInfo);
-
 // req.user.id로 현재 로그인된 회원 정보 수정
-router.put("/", modifyUser);
+router.put("/", upload.single('img'), modifyUser);
 
 // req.user.id로 현재 로그인된 회원 탈퇴
 router.delete("/", deleteUser);
